f25de96ade19a99a31d514060ed58e87b399333c
pr1zmo <zaidbassari@gmail.com>
2025-07-11 20:51:04 +0100

    Modified span.cpp

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -1,5 +1,6 @@
 #include "Span.hpp"
 #include <iostream>
+#include <limits>
 
 Span::Span()
 	: _N(0), _span(), _size(0) {
@@ -28,16 +29,16 @@ Span::~Span() {
 
 void Span::addNumber(int n){
 	if (this->_size == this->_N)
-		throw std::runtime_error("Span size exceeded\n");
+		throw std::length_error("Span size exceeded\n");
 	this->_span.push_back(n);
 	this->_size++;
 }
 
 int Span::shortestSpan(void) const{
-	int old_res = __INT_MAX__;
+	int old_res = std::numeric_limits<unsigned int>::max();
 	int res;
 	if (this->_size <= 1)
-		throw std::runtime_error("Not enough numbers\n");
+		throw std::logic_error("Not enough numbers\n");
 	for (int i = 0; i < (int)this->_size; i++){
 		for (int k = i + 1 ; k < (int)this->_size; k++)
 		{
@@ -52,7 +53,7 @@ int Span::longestSpan(void) const{
 	int old_res = 0;
 	int res;
 	if (this->_size <= 1)
-		throw std::runtime_error("Not enough numbers\n");
+		throw std::logic_error("Not enough numbers\n");
 	for (int i = 0; i < (int)this->_size; i++){
 		for (int k = i + 1 ; k < (int)this->_size; k++)
 		{
